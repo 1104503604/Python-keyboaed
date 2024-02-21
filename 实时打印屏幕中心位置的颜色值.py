0067518b319b8764422b917d0ac9be91436757dc
jinfeng <1104503604@qq.com>
2024-02-21 11:35:30 +0800

    修改颜色值

--- 实时打印屏幕中心位置的颜色值.py
+++ 实时打印屏幕中心位置的颜色值.py
@@ -5,10 +5,5 @@ while True:
     screen_width, screen_height = pyautogui.size()
     center_x, center_y = screen_width // 2, screen_height // 2
     color = pyautogui.pixel(center_x, center_y)
-    
-    if color == (255, 255, 255):
-        print("白色")
-    elif color == (0, 0, 0):
-        print("黑色")
-    
+    print(f"屏幕中心位置的颜色坐标： ({center_x}, {center_y})，颜色值： {color}")
     time.sleep(1)
